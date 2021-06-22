2457523e27c44c4195b7ad28735fa5654e0e4637
mamadou mouctar diallo <mamadoumouctardiallo256@yahoo.com>
2021-06-22 20:08:54 +0200

    third commit for student

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-const { time } = require('console')
 const express = require('express')
 const app = express()
 const path = require('path')
-const server = require('http').createServer(app)
-
-const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
-
-io.on('connection', socket => {
-    console.log('Is connected Up at', Date.now()/3600)
-    socket.on('msg', data => console.log(data))
-})
 
 const PORT = 5000 || process.env.PORT
 
